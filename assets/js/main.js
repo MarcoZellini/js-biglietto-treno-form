beb5ff18039f364430d9c79f67c86339b958aed1
Marco Zellini <zellinimarco@gmail.com>
2023-08-24 17:10:14 +0200

    Add Milestone 2
    Added output on screen with some css

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,20 +15,24 @@
     MILESTONE 2:
     Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
     Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
-    Questo richiederà un minimo di ricerca.
 */
 
 const distanceElement = document.querySelector('#distance');
 const ageElement = document.querySelector('#age');
-const buttonElement = document.querySelector('#button');
+const submitElement = document.querySelector('#submit');
+const resetElement = document.querySelector('#reset');
 const pricePerKm = 0.21;
 let ticketPrice;
 
 console.log('👉', distanceElement, ageElement, pricePerKm, ticketPrice);
 
-buttonElement.addEventListener('click', function () {
+submitElement.addEventListener('click', function (e) {
+    console.log(e);
+    e.preventDefault(); //serve per non far refreshare la pagina una volta premuto su submit
+
     const userAge = Number(ageElement.value);
     const distance = Number(distanceElement.value);
+    let ticketType = "Biglietto Standard";
 
     if (userAge >= 0 && distance > 0) {
         console.log('❌', distance, 'km', userAge, 'anni');
@@ -37,14 +41,31 @@ buttonElement.addEventListener('click', function () {
 
         if (userAge < 18) {
             ticketPrice -= ticketPrice * 0.2;
+            ticketType = "Biglietto Under18"
         } else if (userAge >= 65) {
             ticketPrice -= ticketPrice * 0.4;
+            ticketType = "Biglietto Over65"
         }
         console.log('⭕ Prezzo finale del biglietto: ', ticketPrice);
+
+        document.querySelector('.passenger_name').innerHTML = "Zellini Marco";
+        document.querySelector('.passenger_age').innerHTML = userAge;
+        document.querySelector('.offer_type').innerHTML = ticketType;
+        document.querySelector('.carriage_number').innerHTML = Math.ceil(Math.random() * 10);
+        document.querySelector('.cp_code').innerHTML = Math.ceil(Math.random() * 90000) + 9999;
+        document.querySelector('.ticket_price').innerHTML = ticketPrice.toFixed(2);
+        
+        document.querySelector('table').classList.add('text-center');
+        document.querySelector('.details').style.display = 'block';
+
     } else {
         console.log("Dati non inseriti correttamente");
     }
 
+});
+
+resetElement.addEventListener('click', function() {
+    document.querySelector('.details').style.display = 'none';
 });
 
 
